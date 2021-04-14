6baea4d80fbc6a00403542d2395ec1a09f835280
julio507 <julio.rosa@universo.univates.br>
2021-04-14 14:55:43 -0300

    ajustado problema de build

--- microservices/core/src/main/java/com/univates/core/model/Device.java
+++ microservices/core/src/main/java/com/univates/core/model/Device.java
@@ -34,4 +34,26 @@ public class Device implements AbstractEntiry {
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    
 }
