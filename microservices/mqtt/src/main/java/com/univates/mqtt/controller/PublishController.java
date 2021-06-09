e3b868bc98dabead3e2c6c41bb49ed317a00311f
julio507 <julio.rosa@universo.univates.br>
2021-06-09 19:35:28 -0300

    ajuste build

--- microservices/mqtt/src/main/java/com/univates/mqtt/controller/PublishController.java
+++ microservices/mqtt/src/main/java/com/univates/mqtt/controller/PublishController.java
@@ -2,8 +2,11 @@ package com.univates.mqtt.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 import com.univates.mqtt.Client;
 
+import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/publish")
 public class PublishController {
 
-    @PostMapping("temp")
+    @PostMapping("controller")
     public void setControllerTemp(@RequestBody String data) {
-        Client.getIntance().publish("1.1.1.2/controller", "hello");
+        Map<String, Object> json = new BasicJsonParser().parseMap(data);
+
+        Client.getIntance().publish( json.get( "ip" ) + "/controller", json.get( "temperature" ).toString() );
     }
 }
