6aa21e1d5396ddff5e00a11ee9216f287cef7144
julio507 <julio.rosa@universo.univates.br>
2021-06-22 00:38:12 -0300

    ajustado erro de compilacao

--- microservices/mqtt/src/main/java/com/univates/mqtt/model/Data.java
+++ microservices/mqtt/src/main/java/com/univates/mqtt/model/Data.java
@@ -2,26 +2,14 @@ package com.univates.mqtt.model;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-import org.dom4j.tree.AbstractEntity;
+public class Data {
 
-@Entity
-public class Data extends AbstractEntity{
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
     private Date date;
     private Double temperature;
     private Double humidity;
 
-    @ManyToOne
-    @JoinColumn( name = "deviceId" )
     private Device device;
 
     public void setId(Long id) {
--- microservices/mqtt/src/main/java/com/univates/mqtt/model/Device.java
+++ microservices/mqtt/src/main/java/com/univates/mqtt/model/Device.java
@@ -2,22 +2,14 @@ package com.univates.mqtt.model;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
-@Entity
 public class Device {
     
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+
     private Long id;
     private String name;
     private String ip;
 
-    @OneToMany
     private List<Data> data;
 
     public Long getId() {
